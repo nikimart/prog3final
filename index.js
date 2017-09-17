d3281fa1eee307cdec0ec072ffbe398f342d33e0
user <user@lab1564.tumo.lab>
2017-09-17 11:33:19 +0400

    add  package.json

--- index.js
+++ index.js
@@ -1,7 +1,72 @@
-console.log("Test");
+var scrapeIt = require("scrape-it");
+var jsonfile = require('jsonfile');
 
-var x = 5; 
+var file = 'data.json';
 
-console.log(x);
+// Promise interface
+scrapeIt("http://ionicabizau.net", {
+    title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+    
+}).then(function(page) {
+    console.log(page);   
+});
+// Callback interface
+scrapeIt("http://ionicabizau.net", {
+    // Fetch the articles
+    articles: {
+        listItem: ".article"
+      , data: {
 
-var y = 6;
+            // Get the article date and convert it into a Date object
+            createdAt: {
+                selector: ".date"
+              , convert: x => new Date(x)
+            }
+
+            // Get the title
+          , title: "a.article-title"
+
+            // Nested list
+          , tags: {
+                listItem: ".tags > span"
+            }
+
+            // Get the content
+          , content: {
+                selector: ".article-content"
+              , how: "html"
+            }
+        }
+    }
+
+    // Fetch the blog pages
+  , pages: {
+        listItem: "li.page"
+      , name: "pages"
+      , data: {
+            title: "a"
+          , url: {
+                selector: "a"
+              , attr: "href"
+            }
+        }
+    }
+
+    // Fetch some other data from the page
+  , title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+}, (err, page) => {
+    console.log(err || page);
+    jsonfile.writeFile(file, page,{flag:'a',spaces:2},function(err){
+        console.error(err);
+    }) ; 
+});
