bb9113faf80ce04f09795225091a5a3e4d0f7683
user <user@lab1563.tumo.lab>
2017-10-01 13:56:06 +0400

    add firstlala

--- scraper.js
+++ scraper.js
@@ -4,34 +4,46 @@ var file = 'datax.json';
 
 const scrapeIt = require("scrape-it");
 
+var arr = { articles: [] };
+
 var i = 1;
 
 function q(i) {
-    if(i <= 20)
-    {
+    if (i <= 20) {
         var fs = require("fs");
 
-    scrapeIt("http://www.imdb.com/search/title?groups=top_1000&sort=user_rating&view=simple&page=" + i + "&ref_=adv_nxt", {
+        scrapeIt("http://www.imdb.com/search/title?groups=top_1000&sort=user_rating&view=simple&page=" + i + "&ref_=adv_nxt", {
 
-        articles: {
-            listItem: ".lister-item",
-            data: {
+            articles: {
+                listItem: ".lister-item",
+                data: {
 
-                title: "span > span:nth-child(2) > a",
+                    title: "span > span:nth-child(2) > a",
 
-                rating: ".col-imdb-rating > strong",
-                year: ".text-muted",
-                ranking: ".text-primary",
+                    rating: ".col-imdb-rating > strong",
+                    year: ".text-muted",
+                    ranking: ".text-primary",
 
+                }
             }
-        }
-    }, (err, page) => {
-        jsonfile.writeFile(file, page, { spaces: 2 }, function (err) {
-            console.error(err)
-        })
-    });
-    setTimeout(q(),3000);
-    i++;
+        }, (err, page) => {
+            arr.articles.push(...page.articles);
+
+            if (i == 20) {
+                jsonfile.writeFile(file, arr, { spaces: 2 }, function (err) {
+                    console.error(err);
+                });
+            } else {
+                setTimeout(
+                    function () {
+                        console.log(arr);
+                        i++;
+                        q(i);
+                    }, 3000);
+            }
+        });
+
     }
-    
+
 }
+q(i)
